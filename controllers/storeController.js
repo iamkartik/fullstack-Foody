44bf063b739c850a955a0454d40a7623a623fb70
iamkartik <kartiksharma1891@gmail.com>
2017-09-03 21:55:27 +0530

    code to add the data into the database using async await  and using fuction composition to catch errors

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,10 @@
 // exports is a global variable 
 // whatever follows exports can be imported in another file
+const mongoose = require('mongoose');
+// Store is already imported in start.js
+// now is available to mongoose 
+const Store = mongoose.model('Store');
+
 
 // index page render
 exports.homePage = (req,res)=>{
@@ -12,6 +17,10 @@ exports.addStore = (req,res)=>{
     res.render('editStore',{title:'Add a new Store !'});
 };
 
-exports.createStore = (req,res)=>{
-    res.json(req.body);
+exports.createStore = async (req,res)=>{
+    // because we are using a strict schema 
+    // any other params in req.body will be rejected
+    const store = new Store(req.body);
+    await store.save();
+    res.redirect('/');
 }
